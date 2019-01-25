ad563592ae5cd34dd2717a5a6d1f6f4449fe325c
Peter González Roa <41924660+peterglezroa@users.noreply.github.com>
2019-01-25 16:23:23 -0600

    Laboratorio 3
    Este es el laboratorio 3 teóricamente terminado. Sólo faltaría que las tarjetas se ajusten automáticamente

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,5 +1,5 @@
-  <footer class="page-footer color1">
-    <div class="footer-copyright">
+  <footer class="page-footer color1-07">
+    <div class="footer-copyright color1">
         <div class="container row">
             <div class="col l6 m6 s4 center">PEDRO LUIS GONZÁLEZ ROA</div>
             <div class="col l6 m6 s8 center">A01651517</div>
